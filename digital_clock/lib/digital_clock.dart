828f5c08a8b9305c0752fd231e3dc1badf3e4f4f
Mehul Gupta <MehulGupta@Mehuls-MacBook-Pro.local>
2020-01-19 08:46:39 +0530

    feat(DigitalClock) Updated boilerplate with my designs

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -14,6 +14,8 @@ enum _Element {
   shadow,
 }
 
+final List<String> _Backgrounds = ["red", "yellow", "blue", "black"];
+
 final _lightTheme = {
   _Element.background: Color(0xFF81B3FE),
   _Element.text: Colors.white,
@@ -41,6 +43,10 @@ class DigitalClock extends StatefulWidget {
 class _DigitalClockState extends State<DigitalClock> {
   DateTime _dateTime = DateTime.now();
   Timer _timer;
+  var _temperature = '';
+  var _temperatureRange = '';
+  var _condition = '';
+  var _location = '';
 
   @override
   void initState() {
@@ -69,64 +75,115 @@ class _DigitalClockState extends State<DigitalClock> {
 
   void _updateModel() {
     setState(() {
-      // Cause the clock to rebuild when the model changes.
+      _temperature = widget.model.temperatureString;
+      _temperatureRange = '(${widget.model.low} - ${widget.model.highString})';
+      _condition = widget.model.weatherString;
+      _location = widget.model.location;
     });
   }
 
   void _updateTime() {
     setState(() {
       _dateTime = DateTime.now();
-      // Update once per minute. If you want to update every second, use the
-      // following code.
       _timer = Timer(
         Duration(minutes: 1) -
             Duration(seconds: _dateTime.second) -
             Duration(milliseconds: _dateTime.millisecond),
         _updateTime,
       );
-      // Update once per second, but make sure to do it at the beginning of each
-      // new second, so that the clock is accurate.
-      // _timer = Timer(
-      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
-      //   _updateTime,
-      // );
     });
   }
+
+  String _backgroundMapper(String hourIn24HourFormat, backgrounds) {
+    final hour = int.parse(hourIn24HourFormat);
+    int index = 0;
+    if (hour <= 6) {
+      index = 0;
+    } else if (hour <= 12) {
+      index = 1;
+    } else if (hour <= 18) {
+      index = 2;
+    } else {
+      index = 3;
+    }
+    return 'assets/images/${backgrounds[index]}.png';
+  }
+
+  String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).brightness == Brightness.light
         ? _lightTheme
         : _darkTheme;
-    final hour =
-        DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
-    final minute = DateFormat('mm').format(_dateTime);
-    final fontSize = MediaQuery.of(context).size.width / 3.5;
-    final offset = -fontSize / 7;
-    final defaultStyle = TextStyle(
+
+    final hourFormat = widget.model.is24HourFormat ? 'HH' : 'hh';
+    final time = DateFormat('$hourFormat mm').format(_dateTime);
+    final date = DateFormat('EEEE, MMMM d, y').format(_dateTime);
+    final backgroudImage =
+        _backgroundMapper(DateFormat('HH').format(_dateTime), _Backgrounds);
+    final defaultFontSize = MediaQuery.of(context).size.width;
+    final timeStyle = TextStyle(
       color: colors[_Element.text],
-      fontFamily: 'PressStart2P',
-      fontSize: fontSize,
-      shadows: [
-        Shadow(
-          blurRadius: 0,
-          color: colors[_Element.shadow],
-          offset: Offset(10, 0),
-        ),
-      ],
+      fontSize: defaultFontSize / 3.5,
+    );
+    final dateStyle = TextStyle(
+      color: colors[_Element.shadow],
+      fontSize: defaultFontSize / 32,
+    );
+    final customTheme = Theme.of(context).brightness == Brightness.light
+        ? Theme.of(context).copyWith(
+            // Hour hand.
+            primaryColor: Color(0xFF4285F4),
+            // Minute hand.
+            highlightColor: Color(0xFF8AB4F8),
+            // Second hand.
+            accentColor: Color(0xFF669DF6),
+            backgroundColor: Color(0xFFD2E3FC),
+          )
+        : Theme.of(context).copyWith(
+            primaryColor: Color(0xFFD2E3FC),
+            highlightColor: Color(0xFF4285F4),
+            accentColor: Color(0xFF8AB4F8),
+            backgroundColor: Color(0xFF3C4043),
+          );
+
+    final weatherInfo = DefaultTextStyle(
+      style: TextStyle(color: customTheme.primaryColor),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(_location),
+          Row(
+            children: <Widget>[
+              Text(capitalize(_condition)),
+              Text(', '),
+              Text(_temperature)
+            ],
+          ),
+        ],
+      ),
     );
 
     return Container(
-      color: colors[_Element.background],
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(backgroudImage),
+          fit: BoxFit.cover,
+        ),
+      ),
       child: Center(
-        child: DefaultTextStyle(
-          style: defaultStyle,
-          child: Stack(
-            children: <Widget>[
-              Positioned(left: offset, top: 0, child: Text(hour)),
-              Positioned(right: offset, bottom: offset, child: Text(minute)),
-            ],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            DefaultTextStyle(style: dateStyle, child: Text(date)),
+            DefaultTextStyle(
+                style: timeStyle,
+                child: Text(time, textAlign: TextAlign.center)),
+            weatherInfo
+          ],
         ),
       ),
     );
